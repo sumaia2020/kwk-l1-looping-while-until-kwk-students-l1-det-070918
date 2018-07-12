c26978e9a2fff3deaca74f90ce941263249ce62e
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-12 00:37:57 +0000

    Done.

--- until.rb
+++ until.rb
@@ -1,13 +1,18 @@
 def using_until
-        
+      
         levitation_force =6
        until levitation_force == 10 do
          puts "Wingardium Leviosa"
       levitation_force+= 1
-      
-
-
 end 
 end
 using_until
 
+def using_while
+	levitation_force = 6
+	while levitation_force <=11 do
+	  puts"Wingardium Leviosa"
+	levitation_force+=1
+end
+end
+using_while
