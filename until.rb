f694cfca9779027366ab272dd089c506c2210254
sumaia2020 <sbalghaiti@hawkmail.hfcc.edu>
2018-07-12 00:59:34 +0000

    automatically backed up by learn

--- until.rb
+++ until.rb
@@ -1,16 +1,7 @@
-def using_until
-      
-        levitation_force =6
-       until levitation_force == 10 do
-         puts "Wingardium Leviosa"
-      levitation_force+= 1
-end 
-end
-using_until
 
 def using_while
 	levitation_force = 6
-	while levitation_force <=11 do
+	while levitation_force >=11 
 	  puts"Wingardium Leviosa"
 	levitation_force+=1
 end
